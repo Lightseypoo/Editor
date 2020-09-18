41fea1bc7dc6183e0a4821353cd93254d796f726
Layla A <lights@reachinghalcyon.com>
2020-09-18 12:36:09 -0400

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard"
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2020,
+    sourceType: "module"
+  },
+  plugins: [
+    "react"
+  ],
+  rules: {
+    camelcase: "error",
+    quotes: ["error", "double"],
+    "jsx-quotes": "error",
+    semi: "error",
+    "no-var": "error",
+    "comma-dangle": "error",
+    "comma-spacing": "error",
+    indent: "error",
+    "multiline-comment-style": "error",
+    "no-multiple-empty-lines": "error",
+    "capitalized-comments": "error",
+    "no-useless-return": "error",
+    "prefer-promise-reject-errors": 0
+  }
+}
